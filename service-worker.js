64c281cc91ddd8f474c4f2d07053285404abc317
daedalias <pyrsonists@gmail.com>
2026-09-22 01:26:46 -0700

    v18 - cache refresh

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME =
-    "recipe-viewer-v1";
+    "recipe-viewer-v10";
 
 const FILES_TO_CACHE = [
     "./",
@@ -13,6 +13,8 @@ self.addEventListener(
     "install",
     event => {
 
+        self.skipWaiting();
+
         event.waitUntil(
             caches.open(
                 CACHE_NAME
@@ -24,6 +26,35 @@ self.addEventListener(
                     )
             )
         );
+    }
+);
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+            caches.keys()
+            .then(
+                keys =>
+                    Promise.all(
+                        keys
+                            .filter(
+                                key =>
+                                    key !==
+                                    CACHE_NAME
+                            )
+                            .map(
+                                key =>
+                                    caches.delete(
+                                        key
+                                    )
+                            )
+                    )
+            )
+        );
+
+        self.clients.claim();
     }
 );
 
